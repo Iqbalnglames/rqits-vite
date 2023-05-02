47e309a7ebf9616c2463b8b336d9b170028dfa5e
iqbalnglames <mi06514@gmail.com>
2023-05-02 15:10:10 +0700

    host to https, absen feature to 70%

--- src/assets/Pages/Absen.jsx
+++ src/assets/Pages/Absen.jsx
@@ -2,34 +2,34 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Absen = () => {
-    const [result, setResult] = useState('')
-    
-    const tambahAbsen = async() => {
-        setResult('silahkan scan')
-        const nfc = navigator.nfc
-          try {
-            // menunggu NFC tag ditemukan
-            const tag = await nfc.scan();
+  const [logs, setLogs] = useState([]);
+
+  const handleScanButtonClick = async () => {
+    setLogs([...logs, "User clicked scan button"]);
+
+    try {
+      const ndef = new NDEFReader();
+      await ndef.scan();
+      setLogs([...logs, "> Silahkan scan kartu anda"]);
+
+      ndef.addEventListener("readingerror", () => {
+        setLogs([...logs, "Argh! Cannot read data from the NFC tag. Try another one?"]);
+      });
+
+      ndef.addEventListener("reading", ({  serialNumber }) => {
+        setLogs([...logs, `user dengan serial Number: ${serialNumber} berhasil absen`]);
         
-            // membaca data dari NFC tag
-            const ndef = await tag.readNDEF();
-            const message = ndef[0].message;
-        
-            //mengirim ke state hook
-            setResult(`Pegawai dengan Nomer Pegawai ${message} berhasil melakukan absensi.`)
-             
-          } catch (error) {
-            console.error(error);
-            if(error){
-                setResult('NFC tidak ditemukan')
-            }
-          }
-      
+      });
+    } catch (error) {
+      setLogs([...logs, "Argh! " + error]);
     }
+  };
+      
+    
     return (
         <div className="mt-28">
-           <button onClick={tambahAbsen}>Absen Dimari</button> 
-           <h1>{result}</h1>
+           <button onClick={handleScanButtonClick}>Absen Dimari</button> 
+           <h1>{logs} </h1>
            <Link to={'/user'}>Kembali</Link>
         </div>
     )
